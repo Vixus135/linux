7720fd233fa89744979583a1a4b57fc6c3e546a5
Vixus135 <andreevavika0506@mail.ru>
2025-09-18 18:09:35 +0300

    Новая команда с выводом текста

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main() {
     printf("╔══════════════════════════════════╗\n");
     printf("║          LEVEL 1: HELLO WORLD    ║\n");
-    printf("║                                  ║\n");
+    printf("║                              ║\n");
     printf("║   • Задача: Найти священный GCC  ║\n");
     printf("║   • Босс:   Memory Leak Dragon   ║\n");
     printf("║   • Награда: Бесконечный кофе    ║\n");
