324161d6b68566d9c73d83464052b1263ad28e27
Vixus135 <andreevavika0506@mail.ru>
2025-09-18 19:27:35 +0300

    Новый уровень

---
+++ pr3/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("╔══════════════════════════════════════════╗\n");
+    printf("║        LEVEL 1.5: HELLO WORLD AGAIN      ║\n");
+    printf("║                                          ║\n");
+    printf("║   • Задача: Пройти код-ревью у Гэндальфа ║\n");
+    printf("║   • Босс:    Buffer Overflow Balrog      ║\n");
+    printf("║   • Награда: Магический printf()         ║\n");
+    printf("╚══════════════════════════════════════════╝\n");
+    return 0;
+}
